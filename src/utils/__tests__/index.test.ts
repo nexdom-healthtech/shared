9cd61f157c1c2cbcf0c315263318bc089dc18984
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-07-08 20:16:23 +0000

    test(utils): validate number utils

--- src/utils/__tests__/index.test.ts
+++ src/utils/__tests__/index.test.ts
@@ -3,5 +3,9 @@ import utils from "@/utils/index.ts";
 describe("utils", () => {
   it("should be defined", () => {
     expect(utils).not.toBeUndefined();
+    expect(utils.text).not.toBeUndefined();
+    expect(utils.event).not.toBeUndefined();
+    expect(utils.dateTime).not.toBeUndefined();
+    expect(utils.number).not.toBeUndefined();
   });
 });
