87425ef2ecf86f08c3399b4f4c2b7cb99addfd76
@rafael.perini <rafael.perini@nexdom.tec.br>
2026-07-08 13:48:47 +0000

    test(utils): add validations to check text methods

---
+++ src/utils/__tests__/text.test.ts
@@ -0,0 +1,47 @@
+import { shrinkText, toCamel, toKebab, toSentence, toTitle } from "@/utils/text.ts";
+
+describe("text", () => {
+  describe("toKebab", () => {
+    it("should transform any text into kebab case", () => {
+      const text = "Hello World";
+      const result = toKebab(text);
+      expect(result).toBe("hello-world");
+    });
+  });
+
+  describe("toCamel", () => {
+    it("should transform any text into camel case", () => {
+      const text = "Hello World";
+      const result = toCamel(text);
+      expect(result).toBe("helloWorld");
+    });
+  });
+
+  describe("toTitle", () => {
+    it("should transform any text into title case", () => {
+      const text = "hello world";
+      const result = toTitle(text);
+      expect(result).toBe("Hello World");
+    });
+  });
+
+  describe("toSentence", () => {
+    it("should transform any text into sentence case", () => {
+      const text = "hello world";
+      const result = toSentence(text);
+      expect(result).toBe("Hello world");
+    });
+  });
+
+  describe("shrinkText", () => {
+    it("should return only the first and last words", () => {
+      const text1 = "hello world, this is a test";
+      const result1 = shrinkText(text1);
+      expect(result1).toBe("hello test");
+
+      const text2 = "hello";
+      const result2 = shrinkText(text2);
+      expect(result2).toBe("hello");
+    });
+  });
+});
