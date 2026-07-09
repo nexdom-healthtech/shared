1a46a0a449b3c60ff8b253342226aa45eae75bc5
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-07-09 12:42:34 +0000

    style: improve utils exports

--- src/utils/__tests__/index.test.ts
+++ src/utils/__tests__/index.test.ts
@@ -1,11 +1,8 @@
-import utils from "@/utils/index.ts";
+import * as utils from "@/utils/index.ts";
 
 describe("utils", () => {
   it("should be defined", () => {
     expect(utils).not.toBeUndefined();
-    expect(utils.text).not.toBeUndefined();
-    expect(utils.event).not.toBeUndefined();
-    expect(utils.dateTime).not.toBeUndefined();
-    expect(utils.number).not.toBeUndefined();
+    expect(Object.keys(utils).length).toBeGreaterThan(0);
   });
 });
