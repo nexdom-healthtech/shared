bb6dc6248a10a79636db01788d52f8b96a6f4be4
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-07-09 12:41:22 +0000

    style: migrate error from utils to models

--- src/utils/__tests__/error.test.ts
+++
@@ -1,9 +0,0 @@
-import SharedError from "@/utils/error.ts";
-
-describe("error", () => {
-  it("should ad a prefix to messages", () => {
-    const message = "Oops! Something went wrong.";
-    const error = new SharedError(message);
-    expect(error.message).toBe(`[shared]: ${message}`);
-  });
-});
