4197e4094efc238664888f0745c8d2088ea97077
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-07-22 12:24:04 +0000

    chore: add config for http request testing

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,6 +1,15 @@
+import { server } from "@mocks/server.ts";
+
 beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
   vi.useFakeTimers();
   vi.spyOn(console, "error").mockImplementation(() => {});
   vi.spyOn(console, "warn").mockImplementation(() => {});
   vi.spyOn(console, "log").mockImplementation(() => {});
 });
+
+beforeEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => server.close());
