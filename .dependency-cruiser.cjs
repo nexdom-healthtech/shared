2222533786b951d0887bd75af9e748e5757d2adc
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-07-23 12:32:54 +0000

    test(depcruise): restrict production dependencies to local or peer deps

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -76,16 +76,15 @@ module.exports = {
       },
     },
     {
-      name: "peer-deps-used",
-      comment:
-        "This module depends on an npm package that is declared as a peer dependency " +
-        "in your package.json. This makes sense if your package is e.g. a plugin, but in " +
-        "other cases - maybe not so much. If the use of a peer dependency is intentional " +
-        "add an exception to your dependency-cruiser configuration.",
+      name: "use-peer-deps",
+      comment: `Since this project is a library, you should prefer to use peer dependencies.`,
       severity: "error",
-      from: {},
+      from: {
+        path: ["src"],
+        pathNot: ["__tests__"],
+      },
       to: {
-        dependencyTypes: ["npm-peer"],
+        dependencyTypesNot: ["local", "npm-peer"],
       },
     },
     {
@@ -93,7 +92,7 @@ module.exports = {
       comment: `The root folders from this project must be models, services or utils only.`,
       severity: "error",
       from: {
-        pathNot: ["src/(__tests__|index|models|services|utils)"],
+        pathNot: ["src/(__tests__|index|models|services|utils)", "mocks"],
       },
       to: {},
     },
