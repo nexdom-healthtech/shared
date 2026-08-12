0f46337c430392aa67742c9ef97e933df9227d54
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-08-12 13:02:00 +0000

    feat(services)!: improve HTTP service to handle headers
    Now, it accepts a parameter options on get and delete.
    
    Old parameter body from post, put and patch was also replaced for options.

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,70 +1,76 @@
 import SharedApiError from "@/models/errors/shared-api-error.ts";
 import { METHODS } from "@/services/enums.ts";
-import type { HttpBody } from "@/services/types.ts";
+import type { HttpOptions } from "@/services/types.ts";
 import type { PartialDeep } from "type-fest";
 
 /**
  * API to trigger REST API requests.
  */
-export default class http {
+export default class Http {
   private constructor() {}
 
   /**
    * Dispatch a GET request.
    * @param uri full URL to request
+   * @param options [optional] object containing request headers
    * @returns a promise resolving the response of the request or throws an error if the request wasn't a success
    */
-  static async get<T>(uri: string): Promise<PartialDeep<T>> {
-    return await this.request<T>(uri, METHODS.GET);
+  static async get<T>(uri: string, options?: Omit<HttpOptions, "body">): Promise<PartialDeep<T>> {
+    return await this.request<T>(uri, METHODS.GET, options);
   }
 
   /**
    * Dispatch a POST request.
    * @param uri full URL to request
-   * @param body option object to be sent on the body of the request
+   * @param options [optional] object containing request body and headers
    * @returns a promise resolving the response of the request or throws an error if the request wasn't a success
    */
-  static async post<T>(uri: string, body?: HttpBody): Promise<PartialDeep<T>> {
-    return await this.request<T>(uri, METHODS.POST, body);
+  static async post<T>(uri: string, options?: HttpOptions): Promise<PartialDeep<T>> {
+    return await this.request<T>(uri, METHODS.POST, options);
   }
 
   /**
    * Dispatch a PUT request.
    * @param uri full URL to request
-   * @param body option object to be sent on the body of the request
+   * @param options [optional] object containing request body and headers
    * @returns a promise resolving the response of the request or throws an error if the request wasn't a success
    */
-  static async put<T>(uri: string, body?: HttpBody): Promise<PartialDeep<T>> {
-    return await this.request<T>(uri, METHODS.PUT, body);
+  static async put<T>(uri: string, options?: HttpOptions): Promise<PartialDeep<T>> {
+    return await this.request<T>(uri, METHODS.PUT, options);
   }
 
   /**
    * Dispatch a PATCH request.
    * @param uri full URL to request
-   * @param body option object to be sent on the body of the request
+   * @param options [optional] object containing request body and headers
    * @returns a promise resolving the response of the request or throws an error if the request wasn't a success
    */
-  static async patch<T>(uri: string, body?: HttpBody): Promise<PartialDeep<T>> {
-    return await this.request<T>(uri, METHODS.PATCH, body);
+  static async patch<T>(uri: string, options?: HttpOptions): Promise<PartialDeep<T>> {
+    return await this.request<T>(uri, METHODS.PATCH, options);
   }
 
   /**
    * Dispatch a DELETE request.
    * @param uri full URL to request
+   * @param options [optional] object containing request headers
    * @returns a promise resolving the response of the request or throws an error if the request wasn't a success
    */
-  static async delete<T>(uri: string): Promise<PartialDeep<T>> {
-    return await this.request<T>(uri, METHODS.DELETE);
+  static async delete<T>(
+    uri: string,
+    options?: Omit<HttpOptions, "body">,
+  ): Promise<PartialDeep<T>> {
+    return await this.request<T>(uri, METHODS.DELETE, options);
   }
 
   private static async request<T>(
     uri: string,
     method: METHODS,
-    body?: HttpBody,
+    { body, headers }: HttpOptions = {},
   ): Promise<PartialDeep<T>> {
     try {
       const requestInit: RequestInit = { method };
       if (body) requestInit.body = JSON.stringify(body);
+      if (headers) requestInit.headers = new Headers(headers);
 
       const response = await fetch(uri, requestInit);
       const json = await this.toJSON<T>(response);
