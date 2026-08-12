e3beec9aa6192d61ff8da1127067f5d73a4e6392
rafael-perini <perini.de.souza.rafael@gmail.com>
2026-08-12 13:02:39 +0000

    test(services): validate HTTP headers

--- mocks/ping/index.ts
+++ mocks/ping/index.ts
@@ -2,12 +2,17 @@ import { http, HttpResponse, type PathParams } from "msw";
 
 export const uri = `https://fake-url.com/ping`;
 export const forbiddenUri = `https://fake-url.com/ping/nothing-here`;
+export const authorization = "Q3liZXJkeW5lIFQtODAw";
 export const response = { Ok: true };
 
 const methods: Array<keyof typeof http> = ["get", "post", "put", "patch", "delete"];
 
 const validHandlers = methods.map((method) =>
   http[method]<PathParams, object>(uri, async ({ request }) => {
+    if (request.headers.get("Authorization") !== authorization) {
+      return new HttpResponse(null, { status: 401 });
+    }
+
     const data = request.body ? await request.json() : {};
     return HttpResponse.json({ ...response, ...data, [method]: true });
   }),
